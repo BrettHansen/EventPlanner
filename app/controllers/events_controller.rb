f1a004ae898a8caebc5df43c1b65cb2313a9f7fe
BrettHansen <hansen.brett7@gmail.com>
2014-10-25 15:58:16 -0700

    Homepage now shows title and content of all events stored in database

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,29 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
+  end
+
+  def show
+
+  end
+
+  def new
 
   end
 
   def create
     Event.create!(params.permit(:title).permit(:content))
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 end
