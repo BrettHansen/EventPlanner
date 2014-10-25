15df36c8b18c35ef7f6ac360aced60218431a64b
BrettHansen <hansen.brett7@gmail.com>
2014-10-25 14:56:21 -0700

    Initial commit.  This is a different project than the one we created at the library.  I was able to get a server running and launch this application, but if it's not working, try installing: https://github.com/hiranpeiris/therubyracer_for_windows and node.js The password is the same as before.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EventPlanner::Application.config.secret_key_base = '3c464afb2ba786804d3fa5364ee4c41048aa8ae0b8c424666498c362feca87c46d3af61d542cb9f292bc4b852f4bf38bf78ef7bced268a6a2002a2c43ecaad0b'
