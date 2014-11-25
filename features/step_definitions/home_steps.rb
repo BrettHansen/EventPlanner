a473b0e87620cf3ac6287dc6008577070bc0230a
Ryan Simmons <ryansimmons112782@gmail.com>
2014-11-25 01:55:31 -0700

    changes

--- features/step_definitions/home_steps.rb
+++ features/step_definitions/home_steps.rb
@@ -2,14 +2,6 @@ Given(/^I load the home page$/) do
   visit "http://localhost:3000"
 end
 
-Given(/^I load the login page$/) do
-  visit "http://localhost:3000/users/sign_in"
-end
-
-Given(/^I load the register page$/) do
-  visit "http://localhost:3000/users/sign_up"
-end
-
 When(/^I fill in a unique email$/) do
   @size = rand(100..1000)
   fill_in "user_email", with: "example#{@size}@example.com"
